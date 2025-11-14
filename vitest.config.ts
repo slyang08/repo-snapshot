c3925a17e66a4932e132775777de26f5a9732072
slyang08 <e2223845@gmail.com>
2025-11-14 17:34:26 -0500

    fix: correct configuration for testing and building

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["tests/**/*.test.ts", "tests/**/*.spec.ts"], // Specifies the directory and filename rules for test files
+
+    exclude: [
+      ...configDefaults.exclude, // Includes default exclusion directories such as node_modules
+      "dist/**", // Excludes the dist directory to avoid test output files
+      "src/**/*.test.ts", // If you want to test only the source code files, and there are ts test files that you don't want to be tested, you can also exclude them
+    ],
     coverage: {
       enabled: true,
       provider: "v8",
       include: ["src/**/*.ts"],
-      exclude: ["tests/**", "node_modules/**"],
+      exclude: ["dist/**", "node_modules/**", "tests/**"],
     },
   },
 });
